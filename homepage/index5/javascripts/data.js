18cacf9fbc4dcbc65ed27568db0b6b6f611833dc
jasonwang911 <13383627670@163.com>
2016-11-14 19:05:36 +0800

    personal website 1.0.0

---
+++ homepage/index5/javascripts/data.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Jason on 2016/11/14.
+ */
+var data=[{
+    title:'jQuery',
+    website:'官网：www.jquery.org',
+    cont:'jQuery是一个快速的，简洁的javaScript库，强大的选择器，事件处理，并且提供了的运动（animate）、ajax和jsonp的封装，兼容各种浏览器，简化了我们代码的书写流程',
+    href:'http://www.jquery.org'
+},{
+    title:'angular',
+    website:'官网：angularjs.org',
+    cont:'angular是一个MVVM框架，能帮我们解决频繁的数据交互、DOM拼接操作的繁琐',
+    href:'https://angularjs.org'
+},{
+    title:'nodejs',
+    website:'官网：www.nodejs.org',
+    cont:'nodejs我刚刚开始了解，目前的前端行业越来越偏重数据交互，和大数据的处理，数据操作的思路和方式方法是每一个前端都需要学习和进步的',
+    href:'https://www.nodejs.org'
+},{
+    title:'zepto',
+    website:'官网：www.zeptojs.cn/',
+    cont:'仅仅只有4k大小的一个手机端jquery的库，谁都会喜欢，当然库变小了，并不是不够用，而是把用处不多的部分省略掉了',
+    href:'http://www.zeptojs.cn/'
+}];
